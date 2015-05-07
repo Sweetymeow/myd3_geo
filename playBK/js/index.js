903e892f74550ae92f0467eca3f3d684bd2f5932
Wendy <honeykatie@qq.com>
2015-05-07 12:58:29 -0400

    save

--- playBK/js/index.js
+++ playBK/js/index.js
@@ -3,19 +3,19 @@ var qrread_data,
     nextindex = 0,
 	strokeWidth = 3;
 
-var width = window.innerHeight,
-    height = window.innerHeight,
+var width = 800,
+    height = 800,
     radius = Math.min(width, height) / 2,
+    //color = d3.scale.category20();
 	color = d3.scale.linear()
 			.domain([0,1,2,3])
 			.range(["#EB6596", "#F7A732", "#75CAE6", "#ADD950"]);
-
 var aniTimers = {start: 32, eachSec: 5},
 	aniIndex = 0;
 
-var pathDelay = 400,
-	textDelay = 500,
-	pathDura = 500;
+var pathDelay = 600,
+	textDelay = 700,
+	pathDura = 1000;
 
 // D3: Global SVG Sunbrust
 var globalsvg;
@@ -27,22 +27,11 @@ var explains = [["The project is visualize data  to present gender-based gaps  i
                 ["explaination part 4. "]
 ];
 
-$('.guideImg').hide();
-//$('div.control').hide();
+//$('.guideImg').hide();
+$('div.control').hide();
 $('div#restartBtn').hide();
 //////function for QR //////
 $(function(){
-    $('#jqmeter-horizontal').jQMeter({
-        goal:'$10,000',
-        raised:'$3000',
-        width:'270px',
-        height:'20px',
-        bgColor:'#dadada',
-        barColor:'#f09246',
-        animationSpeed:1000,
-        displayTotal:false
-    }); // Progress bar - jQMeter plug-in
-    
     /************** Start Button **************/
     $('#startBtn').on('click', function(){
         //alert("click next button");
@@ -73,53 +62,35 @@ $(function(){
     $('div#reader').html5_qrcode(function(data){
 			// 在reader标签实现读取QR数据
 			$('div#read').html(data);
-        
-            if(data === "reload"){
-                window.location.replace(location.href);
-            }else if(data === "Moreinfo"){
-                dataTitle();
-                $('span#divId').timer({   // 启动计时器
-                    duration: aniTimers.start + 's',
-                    callback: function() {
-                        explainTimer();
-                        $('span#divId').timer('remove');
-                        $('span#divId').timer({
-                            duration: aniTimers.eachSec + 's', // 不知道为什么时间加倍
-                            callback: function(){
-                                // show explains for each section
-                                nextExpText();
-                                $('span#divId').timer('reset');
-                                aniIndex++;
-                            },
-                            repeat: true
-                        });
-                    }
-                }); // span#divID Timer
-            }else if(data === "stop"){
-                 $('div#reader').html5_qrcode_stop();
-            }else if(data!= ""){
+            if(data!= "" && !playintro){
                 qrread_data = JSON.parse(data); // 全局变量
-                $(window).scrollTop($('div.map').offset().top);
-                //scroll to div with container
+                $(window).scrollTop($('div.map').offset().top); //scroll to div with container
                 $('html, body').animate({
                     scrollTop: $("div.map").offset().top
                 }, 1000);
-                if(qrread_data){ stackedRadial();  }
+                dataTitle(); // 触发d3动画
                 playintro = true;
 				nextindex++; 
                 console.log("QR Data: " + data);
                 console.log("Get QR Country: " + qrread_data.country + "; and year: " + qrread_data.year);
-                
-                $('span#divId').timer({   // 启动计时器
-                    duration: aniTimers.start + 's',
-                    callback: function() {
-                        console.log("Reload!");
-                        window.location.replace(location.href);
-                        $('span#divId').timer('remove');
-                    }
-                }); // span#divID Timer
             }
-        
+			$('span#divId').timer({   // 启动计时器
+				duration: aniTimers.start + 's',
+				callback: function() {
+					explainTimer();
+					$('span#divId').timer('remove');
+					$('span#divId').timer({
+						duration: aniTimers.eachSec + 's', // 不知道为什么时间加倍
+						callback: function(){
+                            // show explains for each section
+                            nextExpText();
+							$('span#divId').timer('reset');
+							aniIndex++;
+						},
+						repeat: true
+					});
+				}
+			}); // span#divID Timer
 		}, function(error){
 			$('#read_error').html(error);
 		}, function(videoError){ 
@@ -127,202 +98,6 @@ $(function(){
 		} 
 	);// html5_qrcode 获得QR信息
 });
-
-/*///////////////// Stacked Radial 第二部分动画 /////////////////////*/
-function stackedRadial(){ 
-    $('svg.StackedRadial').css("top",0);
-    var textarray = ["Labour", "Wage", "Estimated","Legislators","Professional", "Literacy", "primary edu", ],
-        //formatDay = function(d){ return textarray[d] },
-        formatDay = function(d){ return titlearray[d] },
-        titlearray = ["Labour force  participation",
-                      "Wage equality for  similar work (survey)",
-                      "Estimated earned  income (PPP US$)",
-                      "Legislators senior  officials & managers",
-                      "Professional and  technical workers",
-                      "Literacy rate","Enrollment in  primary education",
-                      "Enrollment in  secondary education",
-                      "Enrollment in  tertiary education",
-                      "Sex ratio at  birth (female/male)",
-                      "Healthy life  expectancy",
-                      "Women in parliament",
-                      "Women in ministerial  positions",
-                      "Years with female  head of state(last 50)"]
-        csvFiles = {
-                    cha:{file:"gg_cha.csv",country:"China"}, chi:{file:"gg_che.csv",country:"Chile"},
-                    col:{file:"gg_col.csv",country:"Colombia"}, fra:{file:"gg_fra.csv",country:"France"},
-                    ice:{file:"gg_ice.csv",country:"Iceland"},
-                    ind:{file:"gg_ind.csv",country:"India"}, jap:{file:"gg_jap.csv",country:"Japan"},
-                    nz:{file:"gg_new.csv",country:"New Zealand"}, phi:{file:"gg_phi.csv",country:"Philippines"},
-                    sou:{file:"gg_sa.csv",country:"South Africa"}, uga:{file:"gg_uga.csv",country:"Uganda"},
-                    uk:{file:"gg_uk.csv",country:"United Kingdom"}, us:{file:"gg_us.csv",country:"United States"} 
-                    };
-    var csvFile = findCSV();
-    console.log(csvFiles);
-    console.log("##### csvFile is: " + csvFile);
-    
-    var outerRadius = height/2 - 50,
-        innerRadius = 120;
-
-    var margin = {top: 20, right: 20, bottom: 30, left: 20};
-
-    var angle = d3.time.scale()
-        .range([Math.PI/2, 2 * Math.PI+Math.PI/2]);
-
-    var radius = d3.scale.linear()
-        .range([innerRadius, outerRadius]);
-
-    var color = d3.scale.ordinal()
-            .domain([0,2])
-            .range([ "#F50057", "#FF80AB", "#FF4081"]);
-
-    var stack = d3.layout.stack()
-        .offset("zero")
-        .values(function(d) { return d.values; })
-        .x(function(d) { return d.time; })
-        .y(function(d) { return d.value; });
-
-    var nest = d3.nest()
-        .key(function(d) { return d.key; });
-
-    var line = d3.svg.line.radial()
-        .interpolate("cardinal-closed")
-        .angle(function(d) { return angle(d.time); })
-        .radius(function(d) { return radius(d.y0 + d.y); });
-
-    var area = d3.svg.area.radial()
-        .interpolate("cardinal-closed")
-        .angle(function(d) { return angle(d.time); })
-        .innerRadius(function(d) { return radius(d.y0); })
-        .outerRadius(function(d) { return radius(d.y0 + d.y); });
-
-    var svg = d3.select("div.map").append("svg")
-        .attr("class", "StackedRadial")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
-
-    d3.csv("data/"+csvFile, type, function(error, data) {
-        console.log(data);
-
-        var layers = stack(nest.entries(data));
-        console.log(layers);
-        
-        // Extend the domain slightly to match the range of [0, 2π].
-        angle.domain([0, d3.max(data, function(d) { return d.time + 1; })]);
-        radius.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
-
-        var layers = svg.selectAll(".layer")
-                      .data(layers)
-                      .enter().append("path")
-                      .attr("class", "layer")
-                      .attr("d", function(d) { return area(0); })
-                      .style("fill", function(d, i) { return color(i); });
-        
-        layers.transition()
-            .duration(3000)
-            .delay(function(d,i){
-                return i*1000;
-            })
-            .attr("d", function(d) { return area(d.values); });
-            
-
-        svg.selectAll(".axis")
-            .data(d3.range(angle.domain()[1]))
-            .enter().append("g")
-            .attr("class", "axis")
-            .attr("fill", "white")
-            .attr("transform", function(d) { return "rotate(" + angle(d) * 180 / Math.PI + ")"; })
-            .call(d3.svg.axis()
-                .scale(radius.copy().range([-innerRadius, -outerRadius]))
-                .orient("left"))
-            .append("text")
-            .attr("y", -innerRadius + 6)
-            .attr("dy", "-16em")
-            .attr("text-anchor", "middle")
-            .text(function(d) { return formatDay(d); })
-            .call(wrap, 60);
-        
-        var textCenter = svg.append("g")
-			.attr("class", "countryName")
-        
-        var countryName = textCenter.append("text")
-			.attr("text-anchor", "middle")
-			.style("font-size", "28px")
-			.attr("x", 0 + "px")
-			.attr("y", -16 + "px")
-			.attr("opacity", 0)
-            .text(qrread_data.country)
-            .call(restart);
-        
-        countryName.transition()
-                .duration(1000)
-                .attr("opacity",1);
-        
-        var stackedText1 = textCenter.append("text")
-			.attr("text-anchor", "middle")
-			.attr("font-size", "24px")
-			.attr("fill", "#ddd")
-			.attr("x", 0 + "px")
-			.attr("opacity", 0)
-            .attr("y", 20 + "px")
-            .text(function(){
-                return "3 years stacked streamgraph";
-            });
-        
-        var stackedText2 = textCenter.append("text")
-			.attr("text-anchor", "middle")
-			.attr("font-size", "24px")
-			.attr("fill", "#ddd")
-			.attr("x", 0 + "px")
-			.attr("opacity", 0)
-            .attr("y", 36 + "px")
-            .text(function(){
-                return "for gender gap scores.";
-            });
-
-        stackedText1.transition()
-                .duration(1000)
-                .delay(500)
-                .attr("opacity",1);
-        
-        stackedText2.transition()
-                .duration(1000)
-                .delay(500)
-                .attr("opacity",1);
-        
-        layers.on("mouseover", function(data,i){
-            console.log(data);
-            var m = d3.mouse(this);
-
-            svg.selectAll(".layer").transition()
-                          .duration(250)
-                          .attr("opacity", function(d, j) { return j != i ? 0.2 : 1;  });
-
-            d3.select('#tooptip')
-                .style('left', m[0]+'px')
-                .style('top', m[1]+'px')
-                .select('#year')
-                .text(data.key)
-                .classed('hidden', false);
-        })
-        .on("mouseout", function(){
-            d3.select('#tooptip').classed('hidden', true);
-            svg.selectAll(".layer").transition()
-                          .duration(250)
-                          .attr("opacity", 1);
-        });
-    }); // d3.csv
-
-    function type(d) {
-      d.time = +d.time;
-      d.value = +d.value;
-      return d;
-    }
-} // 
-
-/*///////////////// Stacked Radial 第二部分动画 /////////////////////*/
-
 
 //******************* Partition/Arc 第一部分的parition动画 *******************// 
 function dataTitle(){    
@@ -509,6 +284,191 @@ function dataTitle(){
 	}
 
 }// dataTitle(); 第一部分的parition动画
+
+/*///////////////// Stacked Radial 第二部分动画 /////////////////////*/
+function stackedRadial(){ 
+    $('svg.StackedRadial').css("top",0);
+    var textarray = ["Labour", "Wage", "Estimated","Legislators","Professional", "Literacy", "primary edu", ],
+        //formatDay = function(d){ return textarray[d] },
+        formatDay = function(d){ return titlearray[d] },
+        titlearray = ["Labour force  participation",
+                      "Wage equality for  similar work (survey)",
+                      "Estimated earned  income (PPP US$)",
+                      "Legislators senior  officials & managers",
+                      "Professional and  technical workers",
+                      "Literacy rate","Enrollment in  primary education",
+                      "Enrollment in  secondary education",
+                      "Enrollment in  tertiary education",
+                      "Sex ratio at  birth (female/male)",
+                      "Healthy life  expectancy",
+                      "Women in parliament",
+                      "Women in ministerial  positions",
+                      "Years with female  head of state(last 50)"]
+        csvFiles = {
+                    cha:{file:"gg_cha.csv",country:"China"}, chi:{file:"gg_che.csv",country:"Chile"},
+                    col:{file:"gg_col.csv",country:"Colombia"}, fra:{file:"gg_fra.csv",country:"France"},
+                    ice:{file:"gg_ice.csv",country:"Iceland"},
+                    ind:{file:"gg_ind.csv",country:"India"}, jap:{file:"gg_jap.csv",country:"Japan"},
+                    nz:{file:"gg_new.csv",country:"New Zealand"}, phi:{file:"gg_phi.csv",country:"Philippines"},
+                    sou:{file:"gg_sa.csv",country:"South Africa"}, uga:{file:"gg_uga.csv",country:"Uganda"},
+                    uk:{file:"gg_uk.csv",country:"United Kingdom"}, us:{file:"gg_us.csv",country:"United States"} 
+                    };
+    var csvFile = findCSV();
+    console.log(csvFiles);
+    console.log("##### csvFile is: " + csvFile);
+    
+    var outerRadius = height/2 -40,
+        innerRadius = 120;
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 50};
+
+    var angle = d3.time.scale()
+        .range([Math.PI/2, 2 * Math.PI+Math.PI/2]);
+
+    var radius = d3.scale.linear()
+        .range([innerRadius, outerRadius]);
+
+    var color = d3.scale.ordinal()
+            .domain([0,2])
+            .range([ "#F50057", "#FF80AB", "#FF4081"]);
+
+    var stack = d3.layout.stack()
+        .offset("zero")
+        .values(function(d) { return d.values; })
+        .x(function(d) { return d.time; })
+        .y(function(d) { return d.value; });
+
+    var nest = d3.nest()
+        .key(function(d) { return d.key; });
+
+    var line = d3.svg.line.radial()
+        .interpolate("cardinal-closed")
+        .angle(function(d) { return angle(d.time); })
+        .radius(function(d) { return radius(d.y0 + d.y); });
+
+    var area = d3.svg.area.radial()
+        .interpolate("cardinal-closed")
+        .angle(function(d) { return angle(d.time); })
+        .innerRadius(function(d) { return radius(d.y0); })
+        .outerRadius(function(d) { console.log(d); return radius(d.y0 + d.y); });
+
+    var svg = d3.select("div.map").append("svg")
+        .attr("class", "StackedRadial")
+        .attr("width", width+strokeWidth*2)
+        .attr("height", height+strokeWidth*2)
+        .append("g")
+        .attr("transform", "translate(" + (width/2+strokeWidth) + "," + (height/2+strokeWidth) + ")");
+
+    d3.csv("data/"+csvFile, type, function(error, data) {
+
+        var layers = stack(nest.entries(data));
+
+        // Extend the domain slightly to match the range of [0, 2π].
+        angle.domain([0, d3.max(data, function(d) { return d.time + 1; })]);
+        radius.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
+
+        var layers = svg.selectAll(".layer")
+                      .data(layers)
+                      .enter().append("path")
+                      .attr("class", "layer")
+                      .attr("d", function(d) { return area(d.values); })
+                      .style("fill", function(d, i) { return color(i); });
+
+        svg.selectAll(".axis")
+            .data(d3.range(angle.domain()[1]))
+            .enter().append("g")
+            .attr("class", "axis")
+            .attr("fill", "white")
+            .attr("transform", function(d) { return "rotate(" + angle(d) * 180 / Math.PI + ")"; })
+            .call(d3.svg.axis()
+                .scale(radius.copy().range([-innerRadius, -outerRadius]))
+                .orient("left"))
+            .append("text")
+            .attr("y", -innerRadius + 6)
+            .attr("dy", "-16em")
+            .attr("text-anchor", "middle")
+            .text(function(d) { return formatDay(d); })
+            .call(wrap, 60);
+        
+        var textCenter = svg.append("g")
+			.attr("class", "countryName")
+        
+        var countryName = textCenter.append("text")
+			.attr("text-anchor", "middle")
+			.style("font-size", "28px")
+			.attr("x", 0 + "px")
+			.attr("y", -16 + "px")
+			.attr("opacity", 0)
+            .text(qrread_data.country)
+            .call(restart);
+        
+        countryName.transition()
+                .duration(1000)
+                .attr("opacity",1);
+        
+        var stackedText1 = textCenter.append("text")
+			.attr("text-anchor", "middle")
+			.attr("font-size", "24px")
+			.attr("fill", "#ddd")
+			.attr("x", 0 + "px")
+			.attr("opacity", 0)
+            .attr("y", 20 + "px")
+            .text(function(){
+                return "3 years stacked streamgraph";
+            });
+        
+        var stackedText2 = textCenter.append("text")
+			.attr("text-anchor", "middle")
+			.attr("font-size", "24px")
+			.attr("fill", "#ddd")
+			.attr("x", 0 + "px")
+			.attr("opacity", 0)
+            .attr("y", 36 + "px")
+            .text(function(){
+                return "for gender gap scores.";
+            });
+
+        stackedText1.transition()
+                .duration(1000)
+                .delay(500)
+                .attr("opacity",1);
+        
+        stackedText2.transition()
+                .duration(1000)
+                .delay(500)
+                .attr("opacity",1);
+        
+        layers.on("mouseover", function(data,i){
+            console.log(data);
+            var m = d3.mouse(this);
+
+            svg.selectAll(".layer").transition()
+                          .duration(250)
+                          .attr("opacity", function(d, j) { return j != i ? 0.2 : 1;  });
+
+            d3.select('#tooptip')
+                .style('left', m[0]+'px')
+                .style('top', m[1]+'px')
+                .select('#year')
+                .text(data.key)
+                .classed('hidden', false);
+        })
+        .on("mouseout", function(){
+            d3.select('#tooptip').classed('hidden', true);
+            svg.selectAll(".layer").transition()
+                          .duration(250)
+                          .attr("opacity", 1);
+        });
+    }); // d3.csv
+
+    function type(d) {
+      d.time = +d.time;
+      d.value = +d.value;
+      return d;
+    }
+} // 
+
+/*///////////////// Stacked Radial 第二部分动画 /////////////////////*/
 
 /*///////////////// Next Button /////////////////////*/
 
